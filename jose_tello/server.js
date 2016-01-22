194b0ba54e86807b5a88ddab6f46ad3ceebd980a
Jose Tello <josectello@gmail.com>
2016-01-21 22:25:37 -0800

    setup basic express app

---
+++ jose_tello/server.js
@@ -0,0 +1,12 @@
+const express    = require('express');
+const app        = express();
+const bodyParser = require('body-parser');
+const mongoose   = require('mongoose');
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/restful_api');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.listen(PORT, () => console.log('Server listening on port ' + PORT));
+
+var PORT = process.env.PORT || 3000;
