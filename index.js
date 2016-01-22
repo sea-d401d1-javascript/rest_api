7db9ad656c0ec1d1bec00851de6bc2bc44216068
ERIKAWHO <Erika@RefreshMediaResources.com>
2016-01-22 09:31:57 -0800

    Setting up files

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Function populates the req.body with JSON data.
+module.exports = exports = (req, res, next) => {
+  let content = '';
+  req.on('data', data => {
+    content += data.toString();
+  });
+  req.on('end', () => {
+    try {
+      const json = JSON.parse(content);
+      req.body = json;
+      next();
+    } catch (e) {
+      res.status(500).send('invalid json');
+    }
+  });
+};
