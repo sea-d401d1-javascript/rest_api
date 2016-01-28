847c7da5703e98472ff197cd51d249c7fe29f3ae
Yueyue07 <xww.qyy@gmail.com>
2016-01-27 20:20:50 -0800

    add auth_routes.js

--- yueyueqin/routes/auth_routes.js
+++ yueyueqin/routes/auth_routes.js
@@ -24,3 +24,17 @@ authRouter.post('/signup', jsonParser, (req, res) => {
   });
 
 });
+
+authRouter.get('/signin', basicHTTP, (req, res) => {
+  User.findOne({'authentication.email':req.basicHTTP.email}, (err, user) => {
+    if (err) {return res.status(200).json({msg: 'invalid basic http'});}
+
+    if(!user) return res.status(200).json({msg: 'user does not exist; please sign up this site first'});
+
+    if(!user.comparePassword(req.basicHTTP.password)) return res.status(200).json({msg: 'incorrect passwor. please enter again'});
+
+    res.json({token: user.generateToken()});
+
+  });
+
+});
