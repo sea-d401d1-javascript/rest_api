d79695b5a0a8b4ff3a1e3b44fd7d0c97a5cf0f73
ERIKAWHO <Erika@RefreshMediaResources.com>
2016-01-28 11:57:14 -0800

    Adding tests

---
+++ test/humans_routes_test.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const expect = chai.expect;
+const request = chai.request;
+const mongoose = require('mongoose');
+process.env.MONGOLABL_URI = 'mongodb://localhost/humans_app_test';
+
+const server = require(__dirname + '/../server');
+const Human = require(__dirname + '/../models/humans');
+
+describe('humans API', () => {
+  before((done) => {
+    server.listen(3000);
+    done();
+});
+
+  after((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+      server.close();
+      done();
+    });
+  });
+
+  it('should be able to GET all humans', (done) => {
+    request('localhost:3000')
+      .get('/humans')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(Array.isArray(res.body)).to.eql(true);
+        done();
+      });
+  });
+
+  it('should create a human with a POST', (done) => {
+      request('localhost:3000')
+        .post('localhost:3000')
+        .send({ firstName: 'test human' })
+        .end((err, res) => {
+          expect(err).to.eql(null);
+          expect(res).to.have.status(200);
+          expect(res.body.firstName).to.eql('test human');
+          expect(res.body).to.have.property('_id');
+          done();
+        });
+    });
+
+    describe('require a human already in db', () => {
+      beforeEach((done) => {
+        Human.create({ firstName: 'test human' }, (err, data) => {
+          if (err) throw err;
+          this.testHuman = data;
+          done();
+        });
+      });
+
+      it('should be able to UPDATE a human', (done) => {
+        request('localhost:3000')
+          .put('/humans/' + this.testHuman._id)
+          .send({ firstName: 'new human name' })
+          .end((err, res) => {
+            expect(err).to.eql(null);
+            expect(res.body.msg).to.eql('Successly updated human');
+            expect(res).to.have.status(200);
+            done();
+          });
+      });
+
+      it('should be able to DELETE a human', (done) => {
+        request('localhost:3000')
+          .delete('/humans/' + this.testHuman._id)
+          .end((err, res) => {
+            expect(err).to.eql(null);
+            expect(res.body.msg).to.eql('Successly deleted human');
+            expect(res).to.have.status(200);
+            done();
+          });
+      });
+    });
+  });
