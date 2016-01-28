28ed906ab81a3a7cd00ea18f8778d619da7e1b3c
Yueyue07 <xww.qyy@gmail.com>
2016-01-27 20:21:08 -0800

    add movies_routes.js

--- yueyueqin/routes/movies_routes.js
+++ yueyueqin/routes/movies_routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const jsonParser = require('body-parser').json();
 const Movie = require(__dirname + '/../models/movie');
 const handleError = require(__dirname + '/../lib/handle_error');
+const jwtAuth =  require(__dirname + '/../lib/jwt_auth');
 
 var movieRouter = module.exports = exports = express.Router();
 
-movieRouter.post('/movies',jsonParser, (req,res) => {
+movieRouter.post('/movies',jwtAuth, jsonParser, (req,res) => {
   //add case sensitive handler
   var newMovie = new Movie(req.body);
+  newMovie.wranglerId = req.user._id;
   newMovie.save((err,data) => {
     if(err) handleError(err,res);
     res.status(200).json(data);
@@ -16,6 +18,14 @@ movieRouter.post('/movies',jsonParser, (req,res) => {
 
 movieRouter.get('/movies',(req,res) => {
   Movie.find({ }, (err,data) => {
+    if(err) return handleError(err,res);
+
+    res.status(200).json(data);
+  });
+});
+
+movieRouter.get('/mymovies', jwtAuth, (req,res) => {
+  Movie.find({wranglerId: req.user._id }, (err,data) => {
     if(err) return handleError(err,res);
 
     res.status(200).json(data);
