afa681f523fa3ee5af43e0fcf7d676ee14bdc0f3
ERIKAWHO <Erika@RefreshMediaResources.com>
2016-01-28 18:09:42 -0800

    Adding dependencies

---
+++ routes/auth_router.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+const jsonParser = require('body-parser').json();
+const Dog = require(__dirname + '/models/dog');
+const handleDBError = require(__dirname + '/../lib/handle_db_error');
+
+var dogRouter = module.exports = exports = express.Router();
+
+dogRouter.get('/dogs', (req, res) => {
+  Dog.find({}, (err, data) => {
+    if (err) return handleDBError(err, res);
+
+    res.status(200).json(data);
+  });
+});
+
+dogRouter.post('/dogs', jsonParser, (req, res) => {
+  var newDog = new Dog(req.body);
+  newDog.save((err, data) => {
+    if (err) return handleDBError(err, res);
+
+    res.status(200).json(data);
+  });
+});
+
+dogRouter.put('/dogs/:id', jsonParser, (req, res) => {
+  var dogData = req.body;
+  delete dogData._id;
+  Dog.update({ _id: req.params.id }, dogData, (err) => {
+    if (err) return handleDBError(err, res);
+
+    res.status(200).json({ msg: 'success' });
+  });
+});
+
+dogRouter.delete('/dogs/:id', (req, res) => {
+  Dog.remove({ _id: req.params.id }, (err) => {
+    if (err) return handleDBError(err, res);
+
+    res.status(200).json({ msg: 'success' });
+  });
+});
--- server.js
+++ server.js
@@ -7,8 +7,11 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/dogs_app_dev')
 
 const dogsRouter = require(__dirname + '/routes/dogs_routes');
 const humansRouter = require(__dirname + '/routes/humans_routes');
+const authRouter = require(__dirname + '/routes/auth_router');
 
-app.use('/api', dogsRouter, humansRouter);
+app.use('/api', dogsRouter);
+app.use('/api', humansRouter);
+app.use(authRouter);
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server up on party port: ' + PORT));
