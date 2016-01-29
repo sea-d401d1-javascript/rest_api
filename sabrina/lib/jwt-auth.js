ae0b4cc68a4fd19d7b4f5087c269492a8d806fe7
SABRINA TEE <sabrinatee@me.com>
2016-01-28 19:52:10 -0800

    Working auth routes

--- sabrina/lib/jwt-auth.js
+++ sabrina/lib/jwt-auth.js
@@ -6,14 +6,18 @@ module.exports = exports = function(req, res, next) {
   try {
     decoded = jwt.verify(req.headers.token, process.env.APP_SECRET || 'changethis');
   } catch (e) {
-    return res.status(401).json({msg: 'Not authenticated'});
+    return res.status(401).json({msg: 'Error authenticating'});
   }
+
+  if (!decoded) return res.status(401).json({msg: 'Could not authenticate'});
+
   Donor.findOne({_id: decoded.id}, (err, user) => {
     if (err) {
       console.log(err);
-      return res.status(401).json({msg: 'Not authenticated'});
+      return res.status(500).json({msg: 'Database error'});
     }
-    if (!user) return res.status(401).json({msg: 'Not authenticated'});
+    if (!user) return res.status(401).json({msg: 'User not found'});
+    console.log(req.user);
     req.user = user;
     next();
   });
--- sabrina/routes/donor_routes.js
+++ sabrina/routes/donor_routes.js
@@ -26,43 +26,6 @@ donorRouter.get('/totalDonors', (req, res) => {
   });
 });
 
-donorRouter.get('/signin', basicHTTP, (req, res) => {
-  Donor.findOne({'authentication.email': req.basicHTTP.email}, (err, user) => {
-    if (err) return handleDBError(err, res);
-
-    if (!user) return res.status(401).json({msg: 'User does not exist'});
-
-    if (!user.comparePassword(req.basicHTTP.password)) return res.status(401).json({msg: 'Incorrect password'});
-
-    res.json({token: user.generateToken()});
-  });
-});
-
-donorRouter.post('/signup', jsonParser, (req, res) => {
-  if (!(req.body.email || '').length) {
-    return res.status(400).json({msg: 'Invalid email'});
-  }
-
-  if (!((req.body.password || '').length > 7)) {
-    return res.status(400).json({msg: 'Please enter a password larger than 7 characters'});
-  }
-
-  Donor.findOne({'authentication.email': req.body.email}, (err, email) => {
-    if (err) return handleDBError(err, res);
-    if (email) return res.status(400).json({msg: 'Email taken - enter a new one'});
-
-    var newDonor = new Donor();
-
-    newDonor.username = req.body.username || req.body.email;
-    newDonor.authentication.email = req.body.email;
-    newDonor.hashPassword(req.body.password);
-    newDonor.save((err, data) => {
-      if (err) return handleDBError(err, res);
-      res.status(200).json({token: data.generateToken()});
-    });
-  });
-});
-
 donorRouter.put('/donors/:id', jsonParser, (req, res) => {
   var donorData = req.body;
   delete donorData._id;
